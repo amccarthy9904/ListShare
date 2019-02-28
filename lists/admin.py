efa7b0c7a0ca418c739823f004c08845da32d003
Aaron McCarthy <amccarthy9904@gmail.com>
2019-02-28 15:18:35 -0800

    remove redundant model Profile, functionality moved to List model

--- lists/admin.py
+++ lists/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from .models import List
-from .models import Profile
+from .models import User
 
 @admin.register(List)
 class ListAdmin(admin.ModelAdmin):
     list_display = ['name', 'owner', 'private']
     #Profile.list_display = []
-    pass
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user']
     pass
--- lists/models.py
+++ lists/models.py
@@ -4,18 +4,12 @@ from django.contrib.auth.models import User
 class List(models.Model):
     PRIVACY_CHOICES = [('0', 'Private'), ('1', 'Public')]
     name = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    created_date = models.DateTimeField()
-    editors = models.ManyToManyField('Profile', related_name='editors', blank='True')
-    owner = models.ForeignKey('Profile', models.SET_NULL, null=True, related_name='owner', blank='True')
-    private = models.CharField(choices=PRIVACY_CHOICES, max_length=1)
+    content = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(null=True)
+    owner = models.ForeignKey(User, related_name='owner_of', null=True, blank=True, on_delete=models.CASCADE)
+    editors = models.ManyToManyField(User, related_name='editor_of', null=True, blank=True)
+    viewers = models.ManyToManyField(User, related_name='viewer_of', null=True, blank=True)
+    private = models.CharField(choices=PRIVACY_CHOICES, max_length=1, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    owns = models.ManyToManyField('List', related_name='owner_of')
-    edits = models.ManyToManyField('List', related_name='editor_of')
-    def __str__(self):
-        return self.user.username
