cb1dbf1c2aaed6b80c5b2a66d5c2d734fb7b5ae1
Aaron McCarthy <amccarthy9904@gmail.com>
2019-03-01 18:14:52 -0800

    renames lists to list, so that it can be passed to template

--- lists/views.py
+++ lists/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.http import Http404
 from .models import List
 
+# TODO: refactor these to use sessions when users are a real thing
+#https://docs.djangoproject.com/en/2.1/topics/http/sessions/
 def home(request):
     lists = List.objects.all()
     return render(request, 'home.html', {'lists':lists})
@@ -10,8 +12,8 @@ def home(request):
 
 def list_detail(request, id):
     try:
-        lists = List.objects.get(id=id)
+        list = List.objects.get(id=id)
     except List.DoesNotExist:
         raise Http404('List not found')
-    return render(request, 'list_detail.html', {'list':list})
+    return render(request, 'list_detail.html', {'list':list}, {'id':id})
     #return HttpResponse('<p>list_detail view wioth id {}</p>'.format(id))
