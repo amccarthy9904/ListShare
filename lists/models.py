ffc537fc7e3378e50110ea740a46a972a5c4f721
Aaron McCarthy <amccarthy9904@gmail.com>
2019-03-07 11:38:02 -0800

    adds item model and allows for a list to be itemized or plaintext

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+def get_user_deleted():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
 class List(models.Model):
     PRIVACY_CHOICES = [('0', 'Private'), ('1', 'Public')]
+    TYPE_CHOICES = [('0', 'Text'), ('1', 'Itemized')]
     name = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
     created_date = models.DateTimeField(null=True)
@@ -10,6 +16,18 @@ class List(models.Model):
     editors = models.ManyToManyField(User, related_name='editor_of', null=True, blank=True)
     viewers = models.ManyToManyField(User, related_name='viewer_of', null=True, blank=True)
     private = models.CharField(choices=PRIVACY_CHOICES, max_length=1, null=True, blank=True)
+    type = models.CharField(choices=TYPE_CHOICES, max_length=1, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Items(models.Model):
+    name = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    link = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(null=True)
+    creator = models.ForeignKey(User, related_name='creator_of', null=True, blank=True, on_delete=models.SET(get_user_deleted))
+    list = models.ForeignKey(List, related_name='items', null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
