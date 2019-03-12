35c8e5a7b3e6b0778396c1100c9a4f16951562dc
Aaron McCarthy <amccarthy9904@gmail.com>
2019-03-12 06:08:05 -0700

    switches login view to django default, implements sign_up view

--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
 from .models import List
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # TODO: refactor these to use sessions when users are a real thing
 #https://docs.djangoproject.com/en/2.1/topics/http/sessions/
@@ -17,3 +20,21 @@ def list_detail(request, id):
         raise Http404('List not found')
     return render(request, 'list_detail.html', {'list':list}, {'id':id})
     #return HttpResponse('<p>list_detail view wioth id {}</p>'.format(id))
+
+#def login(request):
+    #return render(request, 'registration/accounts/login.html')
+    #return HttpResponse('<p>home view</p>')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/sign_up.html', {'form': form})
