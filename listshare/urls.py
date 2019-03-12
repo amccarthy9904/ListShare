09f080334d3ba107d202674fb5368dab0d13e3e0
Aaron McCarthy <amccarthy9904@gmail.com>
2019-03-12 06:52:31 -0700

    adds proper urls for login and signup

--- listshare/urls.py
+++ listshare/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls import re_path
+from django.urls import path, re_path, include
 from lists import views
+from  django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    #path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('sign_up/', views.sign_up, name='sign_up'),
     re_path('lists/(\d+)/', views.list_detail, name='list_detail'),
 ]
