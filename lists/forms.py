504b0e39e8c9a16867bb16e007367ad47275ed86
Aaron McCarthy <amccarthy9904@gmail.com>
2019-04-04 09:08:50 -0700

    removed type from fields because it is not implemented yet

--- lists/forms.py
+++ lists/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import List
 
-class EditListForm(forms.ModelForm):
+class EditorEditListForm(forms.ModelForm):
     class Meta:
         model = List
-        fields = ['name', 'content', 'editors', 'viewers', 'private', 'type']
+        fields = ['content']
+
+
+class OwnerEditListForm(forms.ModelForm):
+    class Meta:
+        model = List
+        fields = ['name', 'content', 'owner', 'editors', 'private']
